55b1dad2303e35290f5ad34866b38230ff4415c1
MrAli001 <aliasad.3128@gmail.com>
2023-01-30 21:07:44 +0330

    Add : CircleIcon

---
+++ src/Components/CircleIcon/CircleIcon.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./CircleIcon.css"
+
+export default function CircleIcon({children}) {
+  return (
+    <div className='icon'>
+      {children}
+    </div>
+  );
+}
