6e876a421ab721a90a19fffb7597e4c686959e9c
MrAli001 <aliasad.3128@gmail.com>
2023-02-01 21:53:40 +0330

    Add : Educational records

---
+++ src/data/personalData.js
@@ -0,0 +1,19 @@
+const education = [
+  {
+    school: "Islamic Azad university",
+    city: "Shahr-e Kord",
+    degree: "Undergraduate",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium officiis, optio provident tenetur deserunt sapiente debitis repellat magnam rem voluptatem!",
+    date: {
+      startYear: 2022,
+      startMonth: "Feb",
+      isPresent: true,
+    },
+    logo: "",
+  },
+];
+
+const employment = null;
+
+export { education, employment };
