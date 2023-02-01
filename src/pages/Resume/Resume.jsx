8e9dbb68558e47b5ddab63ee2fb0e59f12beaf22
MrAli001 <aliasad.3128@gmail.com>
2023-02-01 17:14:32 +0330

    Add : Resume page

---
+++ src/pages/Resume/Resume.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Resume.css";
+
+export default function Resume() {
+  return (
+    <div className='resume-page animate__animated animate__fadeInUp'>
+      
+    </div>
+  );
+}
