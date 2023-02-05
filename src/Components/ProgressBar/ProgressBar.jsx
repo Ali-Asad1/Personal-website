bc925053a22fd4d64828f7bb5a91949ca823286e
MrAli001 <aliasad.3128@gmail.com>
2023-02-05 19:45:55 +0330

    Add : animate progress line

--- src/Components/ProgressBar/ProgressBar.jsx
+++ src/Components/ProgressBar/ProgressBar.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ProgressBar.css";
 
 export default function ProgressBar({ label, percent, children }) {
+  const [percentAge, setPercentAge] = useState(0);
+  useEffect(() => {
+    setPercentAge(percent)
+  }, []);
   return (
     <div className='progress'>
       <div className='progress-label'>
         <span className='progress-title'>{label}</span>
         <span className='progress-icon'>{children}</span>
-        <span className="progress-percent">{percent}%</span>
+        <span className='progress-percent'>{percentAge}%</span>
       </div>
       <div className='progress-container'>
-        <div className='progress-line' style={{ width: `${percent}%` }}></div>
+        <div className='progress-line' style={{ width: `${percentAge}%` }}></div>
       </div>
     </div>
   );
