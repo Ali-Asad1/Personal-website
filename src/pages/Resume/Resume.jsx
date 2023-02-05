210de425a81c8cecca936b5ba03e535e518e9c5b
MrAli001 <aliasad.3128@gmail.com>
2023-02-05 20:17:02 +0330

    Add : skill section

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -2,10 +2,16 @@ import React, { useState } from "react";
 import CircleIcon from "../../Components/CircleIcon/CircleIcon";
 import SectionTitle from "../../Components/SectionTitle/SectionTitle";
 import CompanyItem from "../../Components/CompanyItem/CompanyItem";
+import RadialBar from "../../Components/RadialBar/RadialBar";
+import ProgressBar from "../../Components/ProgressBar/ProgressBar";
+import { skills } from "../../data/skills";
 import { MdSchool } from "react-icons/md";
-import { BsFillBriefcaseFill } from "react-icons/bs";
+import { BsFillBriefcaseFill, BsTools } from "react-icons/bs";
+import { BiCodeAlt } from "react-icons/bi";
+import { FaReact, FaNodeJs } from "react-icons/fa";
 import { education as educationData } from "../../data/personalData";
 import "./Resume.css";
+import SkillTools from "../../Components/SkillTools/SkillTools";
 
 export default function Resume() {
   const [education, setEducation] = useState([...educationData]);
@@ -13,8 +19,8 @@ export default function Resume() {
     <div className='resume-page animate__animated animate__fadeInUp'>
       <div className='history-section'>
         <SectionTitle title='My Resume' />
-        <div className='histories'>
-          <div className='history-column'>
+        <div className='columns-container'>
+          <div className='page-column'>
             <CircleIcon lable='Education' borderLine={true}>
               <MdSchool />
             </CircleIcon>
@@ -29,10 +35,59 @@ export default function Resume() {
               />
             ))}
           </div>
-          <div className='history-column'>
+          <div className='page-column'>
             <CircleIcon lable='Employment-history' borderLine={true}>
               <BsFillBriefcaseFill />
             </CircleIcon>
+          </div>
+        </div>
+      </div>
+      <div className='skills-section'>
+        <SectionTitle title='My Skills' />
+        <div className='columns-container'>
+          <div className='page-column'>
+            <CircleIcon lable='Coding' borderLine={true}>
+              <BiCodeAlt />
+            </CircleIcon>
+            <div className='radial-box'>
+              {skills.coding.map((item) => (
+                <RadialBar serie={item.percent} label={item.label} />
+              ))}
+            </div>
+          </div>
+          <div className='page-column'>
+            <CircleIcon lable='Front-End' borderLine={true}>
+              <FaReact />
+            </CircleIcon>
+            <div className='progress-box'>
+              {skills.frontEnd.map((item) => (
+                <ProgressBar percent={item.percent} label={item.label}>
+                  {item.icon}
+                </ProgressBar>
+              ))}
+            </div>
+          </div>
+          <div className='page-column'>
+            <CircleIcon lable='Back-End' borderLine={true}>
+              <FaNodeJs />
+            </CircleIcon>
+            <div className='progress-box'>
+              {skills.backEnd.map((item) => (
+                <ProgressBar percent={item.percent} label={item.label}>
+                  {item.icon}
+                </ProgressBar>
+              ))}
+            </div>
+          </div>
+          <div className='page-column'>
+            <CircleIcon lable='Tools' borderLine={true}>
+              <BsTools />
+            </CircleIcon>
+            <div className='tools-box'>
+              {skills.tools.map((item) => (
+                <SkillTools label={item.label}>{item.icon}</SkillTools>
+              ))}
+            </div>
           </div>
         </div>
       </div>
