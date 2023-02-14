b37ab3a90df9b7602abb468610a577dab6c0ed03
MrAli001 <aliasad.3128@gmail.com>
2023-02-14 10:15:44 +0330

    Remove : main background

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import "animate.css";
 import NavBar from "./Components/NavBar/NavBar";
 import Profile from "./Components/Profile/Profile";
 import { useRoutes } from "react-router-dom";
@@ -9,22 +8,14 @@ import { routes } from "./router/routes";
 export default function App() {
   const router = useRoutes(routes);
   return (
-    <>
-      {/* <div className='parallelogram' id='one'></div>
-      <div className='parallelogram' id='two'></div>
-      <div className='parallelogram' id='three'></div>
-      <div className='parallelogram' id='four'></div>
-      <div className='parallelogram' id='five'></div>
-      <div className='parallelogram' id='six'></div> */}
-      <div className='main'>
-        <div className='main-container'>
-          <div className='page-container animate__animated animate__zoomIn animate__delay-2s'>
-            {router}
-          </div>
-          <Profile />
-          {/* <NavBar /> */}
+    <div className='main'>
+      <div className='main-container'>
+        <div className='page-container animate__animated animate__zoomIn animate__delay-2s'>
+          {router}
         </div>
+        <Profile />
+        <NavBar />
       </div>
-    </>
+    </div>
   );
 }
